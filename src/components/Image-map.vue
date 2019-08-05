b6ffaed8921f44b6859206a184b2e59d0ae8ae73
zaxlct <zaxlct@foxmail.com>
2019-08-05 11:00:33 +0800

    Update Image-map.vue

--- src/components/Image-map.vue
+++ src/components/Image-map.vue
@@ -1,27 +1,44 @@
 <template>
-<div class="box">
+  <div class="box">
     <div class="container" style="position:relative;">
-        <img :src="imgUrl"/>
+      <img :src="imgUrl" />
 
-        <a
-            v-for="area in position"
-            :key="area.id"
-            :href="`JavaScript:android.returnAndroid()`"
-            @click="clickCell(area.id)"
-            class="area-box"
-            :class="{border: currentAreaId === area.id}"
-            :style="{
+      <a
+        v-for="area in position"
+        :key="area.id"
+        :href="`JavaScript:android.returnAndroid(${area.id})`"
+        @click="clickCell(area.id)"
+        class="area-box"
+        :class="{border: currentAreaId === area.id}"
+        :style="{
                 left: (area.x*(screenWidth/600) || 200) + 'px',
                 top: (area.y*(screenWidth/600) || 200) + 'px',
                 width: area.width*(screenWidth/600) + 'px',
                 height: area.height*(screenWidth/600)  + 'px',
-            }">
-        </a>
+            }"
+      ></a>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
+// const testPosition = [
+//   {
+//     height: 129,
+//     id: "Bq_hJMrJ0",
+//     width: 188.01171875,
+//     x: 166.34375,
+//     y: 103,
+//   },
+//   {
+//     height: 196,
+//     id: "ybiWcbwel",
+//     width: 129.9375,
+//     x: 206.34375,
+//     y: 76.90625,
+//   }
+// ]
+
 export default {
     props: {
         imgUrl: String,
@@ -51,22 +68,22 @@ export default {
 </script>
 
 <style>
-.container{
-    width: 100%;
-    height: 100%;
+.container {
+  width: 100%;
+  height: 100%;
 }
 
-.container img{
-    width: 100%;
-    height: auto;
+.container img {
+  width: 100%;
+  height: auto;
 }
 
-.area-box{
-    position: absolute;
+.area-box {
+  position: absolute;
 }
 
 .border {
-    border: 2px dashed red;
+  border: 2px dashed red;
 }
 </style>
 
