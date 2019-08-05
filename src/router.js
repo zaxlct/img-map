754e5a73efea661886954c6b2b1f2256d9f8ae7b
zaxlct <zaxlct@foxmail.com>
2019-08-05 09:17:46 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,11 +3,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-/**
- * @title 每个页面的 title
- * @requireAuth 是否登录后才能访问
- * @wechatCustom 是否自定义微信分享的内容
- */
+
 const routes = [
   {
     path: '/:pageId',
